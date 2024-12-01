aefe5dbf11ad64dccd4d7c5fcb74cd91a9190910
Md Wahiduzzaman Emon <wahedemon09@gmail.com>
2024-12-01 12:36:51 +0600

    Refactor code to update live URL for backend and frontend API endpoint

--- frontend/src/Base/index.ts
+++ frontend/src/Base/index.ts
@@ -1,1 +1,2 @@
-export const APIURL = "http://localhost:4000/api/v1";
+// export const APIURL = "http://localhost:4000/api/v1";
+export const APIURL = "https://link-sharing-app-tlac.onrender.com/api/v1";
